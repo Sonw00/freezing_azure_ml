eaab882d5e8c7dbe2aff5055aadd35c1bcc46880
Son Woo Young <127472859+Sonw00@users.noreply.github.com>
2025-01-24 10:51:57 +0900

    Update application.py

--- FlaskApp/backend/application.py
+++ FlaskApp/backend/application.py
@@ -269,4 +269,4 @@ def load_model_data():
         return jsonify(result='error', message=str(e))
         
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
